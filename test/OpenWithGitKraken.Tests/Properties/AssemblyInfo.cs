1b972cf95c711e4eaf4246377dc7406485abff43
rwlnd <rwlnddev@outlook.com>
2020-10-02 20:35:09 +0200

    added test project + first test for GitRepository util

---
+++ test/OpenWithGitKraken.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenWithGitKraken.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenWithGitKraken.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1342d90-8566-4e03-baf0-c2771259c8bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/OpenWithGitKraken.Tests/Repositories/When_getting_git_repository.cs
@@ -0,0 +1,30 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OpenWithGitKraken.Utils;
+
+namespace OpenWithGitKraken.Tests.Repositories
+{
+    [TestClass]
+    public class When_getting_git_repository
+    {
+
+        [TestMethod]
+        public void With_project_selected_with_git_repository__Should_return_repository_location()
+        {
+            // Arrange
+            var mockedDTE = new Mock<DTE2>();
+            var mockedProject = new Mock<Project>();
+            mockedProject.Setup(x => x.FullName).Returns("test");
+            var activeSolutionProjects = new[] { mockedProject.Object };
+            mockedDTE.Setup(x => x.ActiveSolutionProjects).Returns(activeSolutionProjects);
+
+            // Act
+            var result = GitRepository.GetFromSelection(mockedDTE.Object);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
